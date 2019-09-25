87afc95968d1532d2c38b377b5e0fdc9ab4b164f
realhao <hellyhao1988@vip.qq.com>
2019-09-25 16:50:53 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -2,18 +2,28 @@ import { interval, Subject, BehaviorSubject } from 'rxjs';
 import { filter, multicast, refCount, tap } from 'rxjs/operators';
 
 //emit value in sequence every 1 second
-const source = interval(1000);
+// const source = interval(1000);
 //output: 0,1,2,3,4,5....
-const subscribe = source.subscribe(val => console.log(val));
+// const subscribe = source.subscribe(val => console.log(val));
 
 // setTimeout(() => subscribe.unsubscribe(), 10 * 1000);
 
+declare global {
+  // tslint:disable-next-line: interface-name
+  interface Window {
+    publish: any;
+    t: any;
+  }
+}
 
 const publish = new Subject();
 
 window.publish = publish;
 
-source.subscribe((val) => publish.next(val));
+let index = 0;
+setInterval(() => {
+  publish.next(index++);
+}, 1000);
 
 const get = (v) => publish.pipe(
   filter((msg: number) => {
@@ -73,7 +83,6 @@ setTimeout(() => {
     (v) => console.log(`+++ g2 ${v}`)
   )
   console.log(g22, publish);
-  subscribe.unsubscribe()
 
   setTimeout(() => g22.unsubscribe(), 5 * 1000);
 }, 15 * 1000)
